58703ceb2d0fd32c724a3b8a33e57b2bd43153b4
Banko Viktor <bankviktor14@gmail.com>
2021-08-04 19:22:28 +0300

    Обновлена лицензия и .gitignore, изменена кодировка на utf8

--- Attributes/YAUnitsAttribute.cs
+++ Attributes/YAUnitsAttribute.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Yandex.Weather.Attributes
 {
--- YaWeatherData.cs
+++ YaWeatherData.cs
@@ -1,4 +1,4 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 using System.ComponentModel;
 using System.Diagnostics;
 using Yandex.Weather.Data;
